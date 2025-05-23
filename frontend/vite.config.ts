a4eb039cf74fafa3cb68328c3019c0b71fc9303b
jibaXXX <jibril.woorabyte@gmail.com>
2025-05-23 19:51:35 +0200

    ⚙️ build frontend should work

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,11 +4,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      uuid: 'uuid/dist/esm-browser/index.js',
-    },
-  },
   server: {
     allowedHosts: [
       'ffcf-2a01-e0a-1ff-b170-5c51-6cdf-af84-634e.ngrok-free.app',
